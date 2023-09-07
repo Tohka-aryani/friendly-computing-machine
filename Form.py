f2e778270b491752b598183b05d20afc080a58d1
Tohka-aryani <129815399+Tohka-aryani@users.noreply.github.com>
2023-09-07 23:56:46 +0800

    Update Form.py

--- Form.py
+++ Form.py
@@ -1,47 +1,41 @@
 import streamlit as st
 import requests
 
-# Set up your AniList API credentials here
-CLIENT_ID = '14379'
-CLIENT_SECRET = '3FWrJ4T58fJMVzfL0bouefyjRcr2ebc1rTVGp60o'
+# AniList API base URL
+BASE_URL = "https://graphql.anilist.co"
 
-# Define the AniList API endpoint
-ANILIST_API_URL = 'https://graphql.anilist.co'
-
-# Streamlit app title
-st.title('Anime List with Posters')
-
-# Streamlit sidebar for searching anime
-anime_name = st.sidebar.text_input('Enter anime name:')
-if st.sidebar.button('Search'):
-    # Define the GraphQL query to search for anime by name
-    query = '''
-    query ($name: String) {
-        Media (search: $name, type: ANIME) {
-            id
-            title {
-                romaji
-            }
-            coverImage {
-                medium
-            }
-        }
+# AniList API query for retrieving anime list
+query = """
+{
+  Page(page: 1, perPage: 10) {
+    media(type: ANIME) {
+      id
+      title {
+        romaji
+      }
+      description
+      coverImage {
+        medium
+      }
     }
-    '''
+  }
+}
+"""
 
-    # Define variables for the query
-    variables = {
-        'name': anime_name
-    }
+# Function to fetch anime data from AniList API
+def fetch_anime_data():
+    response = requests.post(BASE_URL, json={"query": query})
+    return response.json()
 
-    # Make a POST request to the AniList API
-    response = requests.post(ANILIST_API_URL, json={'query': query, 'variables': variables})
-    data = response.json()
+# Streamlit app
+def main():
+    st.title("Anime List")
+    anime_data = fetch_anime_data()
 
-    # Display the list of anime with posters
-    for anime in data['data']['Media']:
-        st.write(f"**{anime['title']['romaji']}**")
-        st.image(anime['coverImage']['medium'], use_column_width=True)
+    for anime in anime_data["data"]["Page"]["media"]:
+        st.write(anime["title"]["romaji"])
+        st.write(anime["description"])
+        st.image(anime["coverImage"]["medium"], use_column_width=True)
 
-# Streamlit footer
-st.text("Powered by AniList")
+if __name__ == "__main__":
+    main()
